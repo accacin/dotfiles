6b8e35b8ebfc6191aa4c99d90888c004e873b9c5
accacin <hello@danbrownlow.uk>
2023-08-02 21:35:53 +0100

    Awesome: Set refresh

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -10,3 +10,7 @@ run "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
 # run "feh" --randomize --recursive --bg-fill ~/Pictures/Wallpapers/ &
 run "picom" -b
 run xautolock -time 10 -locker /home/dan/.config/awesome/lock.sh
+
+if xrandr | grep -q 'HDMI-A-1 connected'; then
+	xrandr --output HDMI-A-1 --mode 3440x1440 --rate 99.98
+fi
